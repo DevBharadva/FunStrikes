1d73ff0b4c61d99f5bd8c372f7693166f3f25073
DevBharadva <devbharadva6@gmail.com>
2025-05-09 16:58:39 +0530

    09-05-2025

--- include/header.php
+++ include/header.php
@@ -53,7 +53,7 @@ $version = "0.4.9";
             <div class="container">
                 <div class="menu-container">
                     <nav class="navbar navbar-expand-md position-fixed">
-                        <a class="navbar-brand" href="/"><img src="/images/logo.png" alt=""></a>
+                        <a class="navbar-brand " href="/"><img src="/images/logo.png" alt=""></a>
                         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                             <span class="navbar-toggler-icon"></span>
                         </button>
